db9725bd37678480da2d844b33fceff5fb08c156
jennybonato <jenny.bonato@studenti.univr.it>
2019-01-14 16:33:43 +0100

    update testo consegna

---
+++ wrapper_parser.py
@@ -0,0 +1,24 @@
+from node import Node
+import re
+
+
+class WrapperParser:
+
+    def __init__(self, data, d):
+        self.dags = []
+        # mi divertirò un mondo <3
+
+    def parse_parentesis(self):
+        return 0
+
+    def parse_not(self):
+        return 0
+
+    def parse_and(self):
+        return 0
+
+    def parse_or(self):
+        return 0
+
+    def parse_implies(self):
+        return 0
